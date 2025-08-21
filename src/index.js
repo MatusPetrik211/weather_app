c65488d257ac08b5d523152edb8bf38fa786ade6
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-08-21 15:26:35 +0200

    feat: Add a async getWeather function that gets the weather data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
 
-document.getElementById("app").textContent = "Hello, Webpack!";
+async function getWeather(city) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?key=YCP4G5D49478WUF9C8T6RWA33`,
+    {
+      mode: "cors",
+    },
+  );
+  const weatherData = await response.json();
+
+  console.log(weatherData);
+}
+
+getWeather("trebisov");
+getWeather("Kosice");
+getWeather("bratislava");
