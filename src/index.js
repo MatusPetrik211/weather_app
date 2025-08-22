57bf1caf3034547c2097039b3abac62b3fa6ff38
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-08-22 18:02:39 +0200

    feat: Create a function that process the json data and create an object with only the data that id be working with

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import "./styles.css";
 
 async function getWeather(city) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?key=YCP4G5D49478WUF9C8T6RWA33`,
-    {
-      mode: "cors",
-    },
-  );
-  const weatherData = await response.json();
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?key=YCP4G5D49478WUF9C8T6RWA33`,
+      {
+        mode: "cors",
+      },
+    );
+    const jsonData = await response.json();
+    console.log(jsonData);
+    const weatherData = createWeatherData(jsonData);
+    console.log(weatherData);
+  } catch {
+    console.error("invalid place");
+  }
+}
 
-  console.log(weatherData);
+function createWeatherData(jsonData) {
+  const temp = jsonData.currentConditions.temp;
+  const conditions = jsonData.currentConditions.conditions;
+  const humidity = jsonData.currentConditions.humidity;
+  const uvIndex = jsonData.currentConditions.uvindex;
+  const visibility = jsonData.currentConditions.visibility;
+  const windspeed = jsonData.currentConditions.windspeed;
+  const date = jsonData.days[0].datetime;
+  const desc = jsonData.description;
+
+  // get the first part of resolved location
+  const location = jsonData.resolvedAddress.split(",")[0];
+
+  // modify the location so that the first letter is uppercase
+  const modLocation = `${location[0].toUpperCase()}${location.substr(1)}`;
+
+  return {
+    temp,
+    conditions,
+    humidity,
+    uvIndex,
+    visibility,
+    windspeed,
+    date,
+    desc,
+    modLocation,
+  };
 }
 
 getWeather("trebisov");
-getWeather("Kosice");
-getWeather("bratislava");
+getWeather("kosice");
+getWeather("bratislav");
+getWeather("fffsdfgreg");
